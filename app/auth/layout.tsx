9c8fe0cc463eb482ce0c8ef9ebf1bb9f4e2880a6
avs-abhishek-lotus <av.sai.abhishek@gmail.com>
2025-12-25 17:16:20 +0530

    pushed the google auth error changes

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,15 +1,22 @@
 // app\auth\layout.tsx
+"use client";
+
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { ModernNavbar } from "@/components/modern/modern-navbar";
 import { ModernFooter } from "@/components/modern/modern-footer";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
+    const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '';
+
     return (
-        <div className="flex flex-col min-h-screen">
-            <ModernNavbar />
-            <main className="flex-1">
-                {children}
-            </main>
-            <ModernFooter />
-        </div>
+        <GoogleOAuthProvider clientId={googleClientId}>
+            <div className="flex flex-col min-h-screen">
+                <ModernNavbar />
+                <main className="flex-1">
+                    {children}
+                </main>
+                <ModernFooter />
+            </div>
+        </GoogleOAuthProvider>
     );
 }
